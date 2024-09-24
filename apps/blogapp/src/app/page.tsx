c00ed4ae9cc730509aabea1765fffdc5cbd2de7b
abhishekritu01 <abhishekritu810219@gmail.com>
2024-09-24 12:55:45 +0530

    first commit

--- apps/blogapp/src/app/page.tsx
+++ apps/blogapp/src/app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Welcome to web!</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-6xl">Welcome to blogapp!</h1>
       <Image src="/nextjs.svg" alt="Next.js Logo" width={200} height={200} />
       <button>Click me</button>
     </div>
